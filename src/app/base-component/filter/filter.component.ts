8cad00ecb5f72bd1a395c0210f93eb0f121aa16b
IAGrekhovFronted <iagrekhov.fronted@gmail.com>
2025-02-09 21:04:43 +0500

    Create component filter

--- src/app/base-component/filter/filter.component.ts
+++ src/app/base-component/filter/filter.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
+import { FilterDataService } from '../../../services/filter-data.service'
 
 @Component({
   selector: 'filter',
-  imports: [],
   templateUrl: './filter.component.html',
-  styleUrl: './filter.component.css'
+  styleUrl: './filter.component.css',
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+  ]
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
+  myControl = new FormControl('');
 
+  constructor(
+    private readonly fetchFilter: FilterDataService
+  ) { }
+
+  options: string[] = [];
+
+  async ngOnInit() {
+    this.options = await this.fetchFilter.getFilterData('projects', 'populate[0]=authors&populate[1]=author_teams')
+  }
 }
